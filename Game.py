736a2285ba3c4f016960eae16dd863f4808f91f9
Rohan Sandhu <rssandhu@perse.co.uk>
2021-04-16 09:29:06 +0000

    End of lesson

--- Game.py
+++ Game.py
@@ -1,18 +1,27 @@
 
 class Game:
-
+    EMPTY = ""
+    P1 = "o"
+    P2 = "x"
+    
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY]*3 for _ in range(3)]
+        self.__player = Game.P1
 
     def __repr__(self):
-        pass
+        return ""
 
     def play(self,row,col):
+        self.__board[row][col] = self.__player
+        self.__player = Game.P1 if self.__player == Game.P2 else Game.P1
         pass
     
     @property
     def winner(self):
-        pass
+        if self.__board[0][0] == Game.P1 and self.__board[0][1] == Game.P1 and self.__board[0][2] == Game.P1:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     print("Testing the program")
--- Ui.py
+++ Ui.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from Game import Game
 
 class Ui(ABC):
 
