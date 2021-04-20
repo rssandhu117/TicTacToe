77844b9dd0a633bf367daf8e7f61902dcfbde23e
Rohan Sandhu <rssandhu@perse.co.uk>
2021-04-20 11:25:06 +0000

    Completed objectives 1-5. End of stage 1

--- Ui.py
+++ Ui.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
 from Game import Game
+from abc import ABC, abstractmethod
 
 class Ui(ABC):
 
@@ -12,17 +12,19 @@ class Gui(Ui):
         pass
 
     def run(self):
-        print("Running the GUI")
         pass
 
 class Terminal(Ui):
     def __init__(self):
-        self.__game = Game()
+        self._game = Game()
 
     def run(self):
-        while not self.__game.winner:
-            print(self.__game)
-            row = int(input("Enter the row:"))
-            col = int(input("Enter the column:"))
-            self.__game.play(row, col)
-        
+        while not self._game.winner:
+            print(self._game)
+            row = int(input("Which row? "))
+            col = int(input("Which column? "))
+            self._game.play(row,col)
+
+        print(self._game)
+        w = self._game.winner
+        print(f"The winner was {w}")
